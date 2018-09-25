ea8af914a17948406ce7c27064bf5452446f273b
bopetkovic <bojan.petkovic@drap.hr>
2018-09-25 22:45:15 +0200

    added remember me button

--- TvShows/LoginViewController.swift
+++ TvShows/LoginViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
+extension UITextField {
+    func addBottomBorders() {
+        self.borderStyle = .none
+        
+        let line = CALayer()
+        let width = CGFloat(1.0)
+        line.borderColor = UIColor(red: 86.7/255.0, green: 87.1/255.0, blue: 87.5/255.0, alpha: 0.5).cgColor
+        line.frame = CGRect(x: 0, y: self.frame.size.height - 1.0, width: self.frame.size.width, height: 1.0)
+        line.borderWidth = width
+        self.layer.addSublayer(line)
+        self.layer.masksToBounds = true
+    }
+}
+
 class LoginViewController: UIViewController {
     var secureTextEntry: Bool = true
+    var rememberMe: Bool = false
 
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var emailInputField: UITextField!
     @IBOutlet weak var passwordInputField: UITextField!
     @IBOutlet weak var secureTextEntryButton: UIButton!
+    @IBOutlet weak var rememberMeButton: UIButton!
+    @IBOutlet weak var rememberMeImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,24 +43,15 @@ class LoginViewController: UIViewController {
     func setupUI() {
         loginButton.layer.cornerRadius = 5.0
         
-        addBottomBorders(parentElement: emailInputField)
-        addBottomBorders(parentElement: passwordInputField)
+        emailInputField.addBottomBorders()
+        passwordInputField.addBottomBorders()
         
         setPasswordFieldState()
         secureTextButtonStyle()
+        setRememberMeImage()
     }
     
-    func addBottomBorders(parentElement: UITextField) {
-        parentElement.borderStyle = .none
-        
-        let borderBottom = UIView()
-        borderBottom.frame.size = CGSize(width: parentElement.frame.size.width, height: 1.0)
-        borderBottom.frame.origin = CGPoint(x: 0, y: parentElement.frame.maxY - 15.0)
-        borderBottom.backgroundColor = UIColor(red: 86.7/255.0, green: 87.1/255.0, blue: 87.5/255.0, alpha: 0.5)
-        parentElement.addSubview(borderBottom)
-    }
-    
-    // MARK: - secure text for password field functionality
+    // MARK: - secure text - password field functionality
     func secureTextButtonStyle() {
         let hideCharacters = UIImage(named: "ic-hide-password")
         let showCharacters = UIImage(named: "ic-characters-hide")
@@ -58,11 +66,26 @@ class LoginViewController: UIViewController {
     func setPasswordFieldState() {
         passwordInputField.isSecureTextEntry = secureTextEntry
     }
-
+    
     @IBAction func changeSecureTextState(_ sender: UIButton) {
         secureTextEntry = !secureTextEntry
         
         secureTextButtonStyle()
         setPasswordFieldState()
+    }
+    
+    // MARK: - remember me
+    @IBAction func rememberMeState(_ sender: UIButton) {
+        rememberMe = !rememberMe
+        setRememberMeImage()
+//        print("Remember me value is \(rememberMe)")
+    }
+    
+    func setRememberMeImage() {
+        if rememberMe == true {
+            rememberMeImage.image = UIImage(named: "ic-checkbox-filled")
+        } else {
+            rememberMeImage.image = UIImage(named: "ic-checkbox-empty")
+        }
     }
 }
